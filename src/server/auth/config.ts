b4afbd4a179000e58d8ac394eb7ba95496d03e2c
mkaiho <mkaiho.cgengo@gmail.com>
2025-03-26 14:45:40 +0000

    modify auth config

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,11 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
+import "next-auth/jwt";
+import CredentialsProvider from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
+import GithubProver from "next-auth/providers/github";
+import GoogleProver from "next-auth/providers/google";
+import { z } from "zod";
 
 import { db } from "@/server/db";
 
@@ -17,6 +22,7 @@ declare module "next-auth" {
       // ...other properties
       // role: UserRole;
     } & DefaultSession["user"];
+    provider: string;
   }
 
   // interface User {
@@ -25,6 +31,25 @@ declare module "next-auth" {
   // }
 }
 
+declare module "next-auth/jwt" {
+  interface DefaultJWT {
+    id?: string | null;
+    provider?: string;
+  }
+  interface JWT extends DefaultJWT {
+    id: string;
+    email: string;
+    provider: string;
+  }
+}
+
+const credentialsInputSchema = z.object({
+  email: z.string().email().min(1),
+  password: z.string().min(1),
+});
+
+type CredentialInput = z.infer<typeof credentialsInputSchema>;
+
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
  *
@@ -32,7 +57,25 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
+    CredentialsProvider({
+      authorize: async (credentials) => {
+        const parsedCredentials = await credentialsInputSchema.parseAsync({
+          email: credentials.email,
+          password: credentials.password,
+        });
+        return {
+          email: parsedCredentials.email,
+          password: parsedCredentials.password,
+        };
+      },
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+    }),
     DiscordProvider,
+    GithubProver,
+    GoogleProver,
     /**
      * ...add more providers here.
      *
@@ -43,14 +86,32 @@ export const authConfig = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  session: {
+    strategy: "jwt",
+  },
+  debug: true,
   adapter: PrismaAdapter(db),
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    jwt: ({ token, user, account }) => {
+      if (user?.id && user?.email) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      if (account?.provider) {
+        token.provider = account.provider;
+      }
+      return { ...token };
+    },
+    session: ({ session, user, token }) => {
+      return {
+        ...session,
+        user: {
+          ...user,
+          id: user?.id || token.id,
+          email: user?.email || token.email,
+        },
+        provider: token.provider,
+      };
+    },
   },
 } satisfies NextAuthConfig;
